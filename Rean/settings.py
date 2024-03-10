2a7c0709c217a2d6079def13ab22f001a7596626
telis <telis0kaps@gmail.com>
2024-03-10 22:05:33 +0000

    settings

--- Rean/settings.py
+++ Rean/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'Rean.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
     }
 }
 
